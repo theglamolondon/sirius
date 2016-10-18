e839278f226bbaf1ba19bf9150e0ef990b1ab2f0
BW.KOFFI <glamolondon@gmail.com>
2016-10-18 11:32:27 +0000

    Mise en place de la solution

---
+++ PME Manager Premium +/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace PME_Manager_Premium__
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+           this.InitializeComponent();
+        }
+    }
+}
---
+++ SiriusLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiriusLibrary
+{
+    public class Class1
+    {
+    }
+}
